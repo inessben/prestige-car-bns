8af7c0b4e5ffab458220b8c39deb83564937da65
iness <ine.benss@gmail.com>
2025-03-11 15:02:07 +0100

    website v1

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// // vite.config.js
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   server: {
+//     port: 3000, // Changez ceci par le port de votre choix
+//     strictPort: true, // Cela forcera Vite à ne pas changer le port si celui-ci est occupé
+//   }
+// });
